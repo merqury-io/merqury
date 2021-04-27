59838b3854e11ae4fa3a5cf0c13fae7c98f6a782
Nick Berliner <nick.berliner96@gmail.com>
2021-04-27 17:14:10 +0100

    Api connected (but returning error still)

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import React, { useState, useEffect } from 'react';
+
 import Head from 'next/head';
 import ChatBubble from '../components/ChatBubble';
 import Navbar from '../components/Navbar';
@@ -5,6 +7,23 @@ import Navbar from '../components/Navbar';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
+
+	const [errors, setErrors] = useState(false);
+	const [emails, setEmails] = useState({});
+
+	useEffect(() => {
+		async function fetchData() {
+			const res = await fetch(`https://api.emailjs.com/api/v1.1/history?user_id=user_gEU8MfLqRcGrWPPBSdAG7&accessToken=9aaa2af0fbb41bc229fb7057b40757f6&page=1&count=50`);
+			res
+				.json()
+				.then(res => setEmails(res))
+				.catch(err => setErrors(err));
+		}
+
+		fetchData();
+		
+	});
+
 	return (
 		<div className={styles.main}>
 			<Head>
@@ -16,7 +35,12 @@ export default function Home() {
 				<Navbar />
 			</div>
 			<div className={styles.container}>
-
+				<span>
+					{JSON.stringify(emails, null, 2)}
+				</span>
+				<span>
+					has error: {JSON.stringify(errors, null, 2)}
+				</span>
 				<ChatBubble />
 			
 			</div>
